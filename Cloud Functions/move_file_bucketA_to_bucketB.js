448223f498b1cf81174d40ef5db3aa8034e9dae4
andrikosrikos <41491190+andrikosrikos@users.noreply.github.com>
2019-05-17 10:58:57 +0200

    Updates

--- Cloud Functions/move_file_bucketA_to_bucketB.js
+++ Cloud Functions/move_file_bucketA_to_bucketB.js
@@ -4,15 +4,14 @@ const Storage = require('@google-cloud/storage');
 // Instantiates a client
 const storage = Storage();
 
+//Define bucket's name
 const srcBucketName = "[BUCKET_A]";
 const destBucketName = "[BUCKET_A]";
 
 exports.moveToBucket = (event, context) => {
   const gcsEvent = event;
-  
-  
-  //Call the copy method
-  
+
+  //Start the moving process
   copyFile(gcsEvent.name, gcsEvent.name);
   
   console.log(`Processing file: ${gcsEvent.name}`);
@@ -22,16 +21,17 @@ exports.moveToBucket = (event, context) => {
 
 async function copyFile(srcFilename, destFilename) {
 
-  	// Copies the file to the other bucket
+     // Copies the file to the other bucket
     storage
     .bucket(srcBucketName)
     .file(srcFilename)
     .copy(storage.bucket(destBucketName).file(destFilename))
     .then(() => {
+	//Log when the file is copied sucesfully
       console.log(
         `gs://${srcBucketName}/${srcFilename} copied to gs://${destBucketName}/${destFilename}.`
       );
-      
+      //Start deleting the file from original bucket
       deleteFile(srcFilename);
     })
     .catch(err => {
@@ -43,7 +43,9 @@ async function copyFile(srcFilename, destFilename) {
 async function deleteFile(srcFilename) {
   	var myBucket = storage.bucket(srcBucketName);
   	var file = myBucket.file(srcFilename);
+	//Delete the file
   	file.delete(function(err, apiResponse) {});
+	//Log that deleting process was sucesful 
 	console.log(`File gs://${srcBucketName}/${srcFilename} is deleted.`);
     //-
     // If the callback is omitted, we'll return a Promise.
